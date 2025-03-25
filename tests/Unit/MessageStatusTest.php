fb23b4847f97317a688e49f7663534fee5a7d538
Brian Alexis Florian <briaan.alexisz@hotmail.com>
2025-03-25 07:52:21 -0600

    fix: Added unit test in MessageStatus model for relations messages and messageHistory

--- tests/Unit/MessageStatusTest.php
+++ tests/Unit/MessageStatusTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Models\Message;
+use App\Models\MessageHistory;
 use App\Models\MessageStatus;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Tests\TestCase;
 
 class MessageStatusTest extends TestCase
@@ -18,4 +21,24 @@ class MessageStatusTest extends TestCase
             'name' => $messageStatus->name,
         ]);
     }
+
+        public function test_it_has_many_messages()
+        {
+            $messageStatus = MessageStatus::factory()->create();
+            $messages = Message::factory()->count(3)->create(['message_status_id' => $messageStatus->id]);
+    
+            $this->assertInstanceOf(HasMany::class, $messageStatus->messages());
+            $this->assertCount(3, $messageStatus->messages);
+            $this->assertTrue($messageStatus->messages->contains($messages->first()));
+        }
+    
+        public function test_it_has_many_message_histories()
+        {
+            $messageStatus = MessageStatus::factory()->create();
+            $messageHistories = MessageHistory::factory()->count(2)->create(['message_status_id' => $messageStatus->id]);
+    
+            $this->assertInstanceOf(HasMany::class, $messageStatus->messageHistories());
+            $this->assertCount(2, $messageStatus->messageHistories);
+            $this->assertTrue($messageStatus->messageHistories->contains($messageHistories->first()));
+        }
 }
