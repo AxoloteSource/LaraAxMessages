05980e0f9bbef16411c63d78c5d419e4dc3fad76
Sergio Lopez <dev.sergio0695@gmail.com>
2025-03-24 19:40:27 -0600

    Config messages
    fix app.php
    
    add use Illuminate\Foundation\Configuration\Middleware;

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,36 @@
 <?php
 
+use App\Core\Middleware\IsAllow;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function (Application $app) {
+            //
+           Route::prefix('/api/v1')
+                ->middleware('api')
+                ->group(base_path('routes/api.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'isAllow' => IsAllow::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        $exceptions->render(function (ValidationException $e) {
+            return Response::error(
+                null,
+                $e->errors(),
+            );
+        });
     })->create();
